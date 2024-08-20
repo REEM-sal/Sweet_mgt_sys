ae639825165a09e7a0002dee103883ad7064206d
YasmineSaad793 <s12112317@stu.najah.edu>
2024-08-20 09:54:46 +0200

    Update Gmail.java

--- src/main/java/najah/edu/Gmail.java
+++ src/main/java/najah/edu/Gmail.java
@@ -6,19 +6,23 @@ import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class Gmail {
-	 public Gmail() {
-	       
-	    }
 
-	 private String email;
+    private String email;
+
+    public Gmail() {
+    }
+
+    public Gmail(String email) {
+        this.email = email;
+    }
+
     public void sendEmail(String to, String subject, String msg) {
         String from = "saady9055@gmail.com";
-        String password = "bcuc mnpd ixrq halr"; 
+        String password = System.getenv("GMAIL_PASSWORD"); 
 
         try {
             Properties properties = System.getProperties();
             properties.put("mail.smtp.host", "smtp.gmail.com");
-
             properties.put("mail.smtp.port", "587");
             properties.put("mail.smtp.auth", "true");
             properties.put("mail.smtp.starttls.enable", "true");
@@ -29,20 +33,19 @@ public class Gmail {
                     return new PasswordAuthentication(from, password);
                 }
             });
-            
+
             session.setDebug(true);
             MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress(from));
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
             message.setSubject(subject);
             message.setText(msg);
+
             Transport.send(message);
+            System.out.println("Email sent successfully");
         } catch (MessagingException m) {
             m.printStackTrace();
         }
-    }
-    public Gmail(String email) {
-        this.email = email;
     }
 
     public void setEmail(String email) {
