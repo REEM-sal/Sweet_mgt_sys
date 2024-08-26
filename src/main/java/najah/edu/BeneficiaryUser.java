d36908f304c79fc0bf7e44d73cfcd85e5b366703
YasmineSaad793 <s12112317@stu.najah.edu>
2024-08-26 18:57:58 +0200

    Update BeneficiaryUser.java

--- src/main/java/najah/edu/BeneficiaryUser.java
+++ src/main/java/najah/edu/BeneficiaryUser.java
@@ -2,7 +2,6 @@ package najah.edu;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
